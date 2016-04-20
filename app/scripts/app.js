9c6d2fbab3e980363bc308bca1480d194e823cfb
Exin <ggaao1994@gmail.com>
2016-04-20 23:07:39 +0800

    等待调色

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,8 +6,25 @@ angular.module('app', [
     'ngMaterial'
   ])
   .config(function($mdThemingProvider) {
+    $mdThemingProvider.definePalette('white', {
+      '50': 'ffffff',
+      '100': 'ffffff',
+      '200': 'ffffff',
+      '300': 'ffffff',
+      '400': 'ffffff',
+      '500': 'ffffff',
+      '600': 'ffffff',
+      '700': 'ffffff',
+      '800': 'ffffff',
+      '900': 'ffffff',
+      'A100': 'ffffff',
+      'A200': 'ffffff',
+      'A400': 'ffffff',
+      'A700': 'ffffff'
+    });
     $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      .accentPalette('orange')
-      .warnPalette('red')
+      //.primaryPalette('light-blue')
+      .accentPalette('light-blue')
+      //.warnPalette('grey')
+      //.backgroundPalette('grey');
   });
