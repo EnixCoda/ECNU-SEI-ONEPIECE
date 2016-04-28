b009c013a8278d4f77024277ac9be467c99a2a6f
Exin <ggaao1994@gmail.com>
2016-04-28 18:40:22 +0800

    添加课程详情到grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,6 +50,7 @@ module.exports = function (grunt) {
           'dist/views/about.html': 'app/views/about.html',
           'dist/views/contribute.html': 'app/views/contribute.html',
           'dist/views/file_preview.html': 'app/views/file_preview.html',
+          'dist/views/lesson_preview.html': 'app/views/lesson_preview.html',
           'dist/views/user_center.html': 'app/views/user_center.html'
         }
       }
