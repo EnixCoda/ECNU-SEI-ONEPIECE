38e9e4f20c460445dac189ac3c981d3e8aed4afc
Exin <exincoda@gmail.com>
2016-09-12 18:12:00 +0800

    style improvement

--- app/components/item.js
+++ app/components/item.js
@@ -2,15 +2,12 @@ angular.module('onepiece')
   .component('item', {
     template: `
       <md-list-item ng-click="explorer.goTo(content, $event)">
-        <div layout layout-align="start center">
-          <md-icon class="material-icons" ng-style="getFileColor(content)">
-            {{content.isDir ? "folder" : getFileIcon(content)}}
-          </md-icon>
-          <div class="left-offset-10 file-name-in-list" ng-class="getContentNameStyle(content)">
-            <p>{{content.name}}</p>
-          </div>
+        <md-icon class="material-icons no-margin" ng-style="getFileColor(content)">
+          {{content.isDir ? 'folder' : getFileIcon(content)}}
+        </md-icon>
+        <div flex class="left-offset-10 file-name-in-list" ng-class="getContentNameStyle(content)">
+          <p class="no-margin">{{content.name}}</p>
         </div>
-        <div flex></div>
         <md-button ng-if="!content.isDir && explorer.path.length > 1" class="md-icon-button" ng-click="downloadFile(content)" layout layout-align="center center">
           <md-tooltip md-direction="left">
             {{content.gettingDownloadLink?'正在获取下载链接':formatFileSize(content)}}
