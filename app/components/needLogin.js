373f2602b2896a745f56d67453c60c96afe1e32b
Exin <exincoda@gmail.com>
2016-10-27 20:11:26 +0800

    center align needLogin component for better layout

--- app/components/needLogin.js
+++ app/components/needLogin.js
@@ -1,7 +1,7 @@
 angular.module('onepiece')
   .component('needLogin', {
     template: `
-      <div ng-show="user.status !== 'ONLINE'">
+      <div ng-if="user.status !== 'ONLINE'" class="center-text">
         <span>请先使用校内数据库账户登录。</span>
         <md-button class="md-raised md-primary" ng-click="showUserCenter()">登陆</md-button>
       </div>
