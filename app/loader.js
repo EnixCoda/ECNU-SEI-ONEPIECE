149328323ec4894afff48c03c33a99687f37dc1d
Exin <exincoda@gmail.com>
2016-10-30 15:51:10 +0800

    remove loading assets animation

--- app/loader.js
+++ app/loader.js
@@ -1,4 +1,4 @@
-var version = '1.0.0';
+var version = '1.0.2';
 
 load('script', '/assets/app.js');
 load('style', '/assets/app.css');
@@ -9,34 +9,18 @@ function load(type, url) {
   if (version === localVersion && item) {
     appendNode(type, item);
   } else {
-    localStorage.removeItem('assets.' + type);
-    localStorage.removeItem('assets.version');
     var request = new XMLHttpRequest();
     request.onreadystatechange = function (event) {
-      if (request.status === 200 && request.readyState === 4) {
+      if (request.readyState === 4) {
+        console.log('in');
         var content = request.responseText;
         appendNode(type, content);
         localStorage.setItem('assets.' + type, content);
         localStorage.setItem('assets.version', version);
       }
     };
-    request.onprogress = function (event) {
-      drawLoadProgress(type, event.loaded, event.total);
-    };
     request.open('get', url);
     request.send();
-  }
-}
-
-function drawLoadProgress(type, loaded, total) {
-  var loaderProgress = document.querySelector('#loader-progress');
-  progress = loaded / total;
-  width = progress * 100 + '%';
-  if (type === 'script') {
-    loaderProgress.style.width = width;
-    if (loaded === total) setTimeout(function () {
-      loaderProgress.style.display = 'none';
-    }, 200);
   }
 }
 
