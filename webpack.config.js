bf0ae3c275c7366cbade5ef59a880b087f569a47
EnixCoda <enixcoda@gmail.com>
2018-04-07 19:26:59 +0800

    feat(React): dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -63,8 +63,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: ['env'],
-              plugins: ['angularjs-annotate'],
+              presets: ['env', 'react'],
             },
           },
         ],
