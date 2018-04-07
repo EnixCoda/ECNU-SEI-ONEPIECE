015d70f05c6eae7a44fc2c6c076757cd5d1b84ca
EnixCoda <enixcoda@gmail.com>
2018-04-08 03:11:37 +0800

    chore: remove old source code

--- src/controllers/main.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular'
-
-export default angular.module('onepiece')
-  .controller('MainController',
-    ($scope, explorer, user, indexLoader) => {
-      $scope.indexLoader = indexLoader
-      indexLoader.load()
-      user.loginWithToken()
-    })
