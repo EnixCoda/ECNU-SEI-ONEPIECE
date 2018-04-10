f8f5def29324f03ee9a3621eb12218fe0d17d15c
EnixCoda <enixcoda@gmail.com>
2018-04-10 22:42:26 +0800

    feat(framework): replace angular with React

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-import angular from 'angular'
+import React from 'react'
 
-export default angular.module('onepiece', ['ngMaterial', 'ngResource'])
-  .config($mdThemingProvider => {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      .accentPalette('orange')
-      .warnPalette('deep-orange')
-      .backgroundPalette('grey')
-    $mdThemingProvider.theme('error-toast')
-    $mdThemingProvider.theme('success-toast')
-    $mdThemingProvider.theme('warning-toast')
-  }).config(($resourceProvider) => {
-    // Don't strip trailing slashes from calculated URLs
-    $resourceProvider.defaults.stripTrailingSlashes = false
-  })
+import './app.css'
 
+
+function App() {
+  return (
+    <div>
+    </div>
+  )
 }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './app'
 
 import './loading.css'
 // import './loadServiceWorker'
+
+ReactDOM.render(<App />, document.querySelector('#app'))
