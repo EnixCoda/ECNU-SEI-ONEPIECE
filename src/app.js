59e5eb75c6df220d88c16ae0fc2ea5a5bf05310a
EnixCoda <enixcoda@gmail.com>
2018-04-10 22:34:18 +0800

    awfwf

--- src/app.js
+++ src/app.js
@@ -15,12 +15,4 @@ export default angular.module('onepiece', ['ngMaterial', 'ngResource'])
     $resourceProvider.defaults.stripTrailingSlashes = false
   })
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/assets/service-worker.js')
-    .then(function() {
-      console.log('service worker registered')
-    }, function(e) {
-      console.log('service worker register failed', e)
-    })
 }
